6ec45e2e50e212d4fe0314e9974c4e61976bcc81
thausou <thauane-souza1@live.com>
2025-06-06 23:58:18 -0300

    Initial commit

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './Aside.module.css';
+
+import logo from './logo.png';
+
+export const Aside = () => {
+  return (<aside className={styles.aside}>
+      <Image src={logo} alt='th dev'/>
+
+  </aside>)
+}
